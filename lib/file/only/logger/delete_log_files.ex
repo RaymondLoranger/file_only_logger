efa8ac295fd8e72825ae0c591e1994da9a8a1f7e
RaymondLoranger <Raymond.Loranger@bell.net>
2022-01-29 16:12:08 -0500

    done

--- lib/file/only/logger/delete_log_files.ex
+++ lib/file/only/logger/delete_log_files.ex
@@ -1,19 +1,19 @@
 defmodule File.Only.Logger.DeleteLogFiles do
-  def __after_compile__(_env, _bytecode) do
-    if File.cwd!() |> String.ends_with?("#{Mix.Project.config()[:app]}") do
-      # %{debug: "./log/debug.log", info: "./log/info.log", ...}
-      paths = %{
-        debug: Application.get_env(:logger, :debug_log)[:path],
-        info: Application.get_env(:logger, :info_log)[:path],
-        warn: Application.get_env(:logger, :warn_log)[:path],
-        error: Application.get_env(:logger, :error_log)[:path]
-      }
+  @moduledoc false
 
-      # Delete each log file...
-      paths
-      |> Map.values()
-      |> Enum.reject(&is_nil/1)
-      |> Enum.each(&File.rm/1)
-    end
+  def __after_compile__(_env, _bytecode) do
+    # => %{debug: "./log/debug.log", info: "./log/info.log", ...}
+    paths = %{
+      debug: :application.get_env(:logger, :debug_log, nil)[:path],
+      info: :application.get_env(:logger, :info_log, nil)[:path],
+      warn: :application.get_env(:logger, :warn_log, nil)[:path],
+      error: :application.get_env(:logger, :error_log, nil)[:path]
+    }
+
+    # Delete each log file...
+    paths
+    |> Map.values()
+    |> Enum.reject(&is_nil/1)
+    |> Enum.each(&File.rm/1)
   end
 end
