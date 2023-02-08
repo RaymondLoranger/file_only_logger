56eba7e4dec1cfb95f4cbbd717497cd762899879
RaymondLoranger <Raymond.Loranger@bell.net>
2023-02-08 15:40:45 -0500

    last commit for version 0.1

--- lib/file/only/logger/proxy/try.ex
+++ lib/file/only/logger/proxy/try.ex
@@ -7,26 +7,21 @@ defmodule File.Only.Logger.Proxy.Try do
 
   alias File.Only.Logger.Log
 
-  @flush true
+  @add_opts flush: false
+  @rmv_opts flush: false
   @times 7
   @wait 10
 
   @doc """
   Removes the console backend. If it fails, will retry up to #{@times} times.
   """
-  @dialyzer {:no_match, remove_console_backend: 0}
   @spec remove_console_backend :: :ok | {:error, atom}
   def remove_console_backend do
-    case Logger.remove_backend(:console, flush: @flush) do
+    case Logger.remove_backend(:console, @rmv_opts) do
       :ok ->
         :ok
 
       {:error, :not_found} ->
-        :ok
-
-      # Can never match per Dialyzer. But just in case...
-      {:error, reason} ->
-        :ok = Log.warn(:unremoved, {@wait, @times, reason, __ENV__})
         remove_console_backend(@times)
     end
   end
@@ -36,7 +31,7 @@ defmodule File.Only.Logger.Proxy.Try do
   """
   @spec add_console_backend :: :ok | {:error, atom}
   def add_console_backend do
-    case Logger.add_backend(:console, flush: @flush) do
+    case Logger.add_backend(:console, @add_opts) do
       {:ok, _pid} ->
         :ok
 
@@ -51,7 +46,6 @@ defmodule File.Only.Logger.Proxy.Try do
 
   ## Private functions
 
-  @dialyzer {:no_unused, remove_console_backend: 1}
   @spec remove_console_backend(non_neg_integer) :: :ok | {:error, atom}
   defp remove_console_backend(0) do
     :ok = Log.warn(:remains_unremoved, {@wait, @times, __ENV__})
@@ -62,7 +56,7 @@ defmodule File.Only.Logger.Proxy.Try do
     Process.sleep(@wait)
     times_left = times_left - 1
 
-    case Logger.remove_backend(:console, flush: @flush) do
+    case Logger.remove_backend(:console, @rmv_opts) do
       :ok ->
         :ok = Log.warn(:now_removed, {@wait, @times - times_left, __ENV__})
 
@@ -82,7 +76,7 @@ defmodule File.Only.Logger.Proxy.Try do
     Process.sleep(@wait)
     times_left = times_left - 1
 
-    case Logger.add_backend(:console, flush: @flush) do
+    case Logger.add_backend(:console, @add_opts) do
       {:ok, _pid} ->
         :ok = Log.warn(:now_added, {@wait, @times - times_left, __ENV__})
 
