36dd928fbec547bc0a84127c11dd949b7e3c7e80
RaymondLoranger <Raymond.Loranger@bell.net>
2024-02-24 20:11:53 -0500

    0.2.27

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule File.Only.Logger.MixProject do
   def project do
     [
       app: :file_only_logger,
-      version: "0.2.26",
+      version: "0.2.27",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       name: "File-Only Logger",
