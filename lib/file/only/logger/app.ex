efee3bd893e33b7dc25dc7ac0ee111d9df2d29d6
RaymondLoranger <Raymond.Loranger@bell.net>
2025-08-13 16:19:42 -0400

    handler-config-done

--- lib/file/only/logger/app.ex
+++ lib/file/only/logger/app.ex
@@ -1,19 +1,33 @@
 defmodule File.Only.Logger.App do
   use Application
+  use PersistConfig
+
+  alias File.Only.Logger.Log
+
+  @logger_config get_env(:logger)
+  @handler_config hd(@logger_config)
+  # :debug_handler...
+  @handler_id elem(@handler_config, 1)
 
   @spec start(Application.start_type(), term) :: {:ok, pid}
   def start(_start_type, _start_args = :ok) do
-    # Prevent console messages...
-    :ok = :logger.set_handler_config(:default, :level, :none)
-
-    # Add debug, info, warning and error handlers...
-    Logger.add_handlers(:file_only_logger)
-    # Returns :ok or {:error, term} where term would likely be:
-    # {:bad_config, {:handler, {:file_only_logger, {:already_exist, _id}}}}
-
-    # Allow console messages...
-    :ok = :logger.set_handler_config(:default, :level, Logger.level())
+    if @handler_id not in :logger.get_handler_ids() do
+      add_handlers(:file_only_logger)
+    end
 
     {:ok, self()}
+  end
+
+  ## Private functions
+
+  # Add debug, info, warning and error handlers...
+  # Returns :ok or {:error, term} where term would likely be:
+  # {:bad_config, {:handler, {:file_only_logger, {:already_exist, _id}}}}
+  @spec add_handlers(Application.app()) :: :ok
+  defp add_handlers(app) do
+    case Logger.add_handlers(app) do
+      :ok -> :ok
+      {:error, reason} -> :ok = Log.error(:handlers, {reason, app, __ENV__})
+    end
   end
 end
---
+++ lib/file/only/logger/log.ex
@@ -0,0 +1,13 @@
+defmodule File.Only.Logger.Log do
+  use File.Only.Logger
+
+  error :handlers, {reason, app, env} do
+    """
+    \nError adding configured handlers...
+    • Call: 'Logger.add_handlers/1'
+    • App argument: #{app}
+    • Reason: #{inspect(reason) |> maybe_break(10)}
+    #{from(env, __MODULE__)}\
+    """
+  end
+end
