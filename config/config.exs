ece55b273f74828b1a4d626af5dfad1f56a6977d
RaymondLoranger <Raymond.Loranger@bell.net>
2025-09-02 18:50:41 -0400

    config-logger-ok?

--- config/config.exs
+++ config/config.exs
@@ -2,8 +2,8 @@ import Config
 
 config :elixir, ansi_enabled: true
 
-import_config "config_logger.exs"
-# For testing purposes only...
+# import_config "config_logger.exs"
 
+# For testing purposes only...
 config :file_only_logger,
   env: "#{Mix.env()} ➔ from #{Path.relative_to_cwd(__ENV__.file)}"
