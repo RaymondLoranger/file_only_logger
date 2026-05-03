82693d1023cbb3d5ddc68cf1ff0e22b501b21aa8
RaymondLoranger <Raymond.Loranger@bell.net>
2026-05-03 16:21:13 -0400

    "0.2.55"

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,8 @@ import Config
 
 config :elixir, ansi_enabled: true
 
+import_config "config_logger.exs"
+
 # For testing purposes only...
 config :file_only_logger,
   env: "#{Mix.env()} ➔ from #{Path.relative_to_cwd(__ENV__.file)}"
